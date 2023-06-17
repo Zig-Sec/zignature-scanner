74d8c4e089df88f5c79310de009d66b6d3a95ca8
SoraTenshi <dream@neoncity.dev>
2023-06-18 01:48:10 +0200

    Add simple Scanner structure

--- src/scanner.zig
+++ src/scanner.zig
@@ -1,3 +1,29 @@
 const std = @import("std");
+const sig = @import("signature.zig");
 
-test "basic scanner" {}
+pub fn Scanner(comptime signature: []const u8) type {
+    const size = comptime std.mem.count(u8, signature, " ") + 1;
+    return struct {
+        const Self = @This();
+
+        /// The Signature to hold
+        signature: sig.Signature(size),
+
+        pub fn init() !Self {
+            return Self{
+                .signature = try sig.Signature(size).init(signature),
+            };
+        }
+    };
+}
+
+test "scanner construction" {
+    _ = try Scanner("AA ?? BB").init();
+    _ = try Scanner("AA ? BB").init();
+    _ = try Scanner("AA CC BB").init();
+}
+
+test "scanner failing construction" {
+    try std.testing.expectError(sig.SignatureError.SizeMismatch, Scanner("AA BB ").init());
+    try std.testing.expectError(sig.SignatureError.InvalidSignatureByte, Scanner("AA x").init());
+}
