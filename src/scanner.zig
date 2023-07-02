70cc8824d586fa450e09f8f508f8a750111a8fde
SoraTenshi <dream@neoncity.dev>
2023-07-02 22:58:21 +0200

    Return the found address from scanner

--- src/scanner.zig
+++ src/scanner.zig
@@ -16,32 +16,38 @@ pub fn Scanner(comptime signature: []const u8) type {
         }
 
         /// Scans the given memory range for the byte sequence
-        pub fn scan(self: Self, start_address: [*]u8, end_address: [*]u8) bool {
+        pub fn scan(self: Self, start_address: [*]u8, end_address: [*]u8) ?usize {
             const total = @intFromPtr(end_address) - @intFromPtr(start_address);
-            var current_index: usize = 0;
+            var watcher = .{ .address = 0x0, .index = 0 };
             var i: usize = 0;
             while (i < total) : (i += 1) {
-                const current = self.signature.bytes[current_index];
+                const current = self.signature.bytes[watcher];
                 // Wildcard is fine, go next
                 if (current.is_wildcard) {
-                    current_index += 1;
+                    // no need to track the watcher's address here
+                    // a wildcard on the first position doesn't matter anyway
+                    watcher.index += 1;
                     continue;
                 }
 
                 // No Wildcard here, so there must be a byte left...
                 const cur_byte = current.byte.?;
-                if (start_address[i] == cur_byte and current_index == (self.signature.bytes.len - 1)) {
-                    return true;
+                if (start_address[i] == cur_byte and watcher.index == (self.signature.bytes.len - 1)) {
+                    return watcher.address;
                 } else if (start_address[i] == cur_byte) {
-                    current_index += 1;
+                    if (watcher.address == 0x0) {
+                        watcher.address = @intFromPtr(&start_address[i]);
+                    }
+                    watcher += 1;
                 } else {
                     // Maybe we just hit the first byte of the signature
                     i -= 1;
-                    current_index = 0;
+                    watcher.index = 0;
+                    watcher.address = 0x0;
                 }
             }
 
-            return current_index.index == (self.signature.bytes.len - 1);
+            return if (watcher.index == (self.signature.bytes.len - 1)) watcher.address else null;
         }
     };
 }
